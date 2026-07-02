93bf37932cd6a209bee3c69065f457abe460fc86
DavidHuijser <davidhuijser@gmail.com>
2026-07-02 16:10:05 +1200

    Initial commit

---
+++ fit_nonlinear.R
@@ -0,0 +1,27 @@
+fit_nonlinear <- function(x, y, start_par) {
+  
+  n <- start_par$n
+  model_type <- start_par$model_type 
+  
+  if (model_type == 'A')
+  {
+      start_par <- c(start_par$a, start_par$b, start_par$c, start_par$A, start_par$B, start_par$freq)
+  }
+  
+  objective_function <- function(par) {
+    y_pred <- predict_model_A(x, par)
+    sum((y - y_pred)^2)
+  }
+  
+  result <- optim(start_par, objective_function)
+  if (model_type == 'A')
+  {
+    result$model <- predict_model_A(x, result$par)
+  }
+  result
+  
+}
+
+
+
+
---
+++ run_script.R
@@ -0,0 +1,25 @@
+# run the main program 
+library(ggplot2)
+source("fit_nonlinear.R")
+source("predict_model.R")
+source("plot_model.R")
+source("pipeline.R")
+
+
+# load the data
+my_data <-read.csv("DATA501_Assignment1_data.csv", row.names = NULL)
+head(my_data)
+x <- my_data$x
+y <- my_data$y
+
+# set initial parameters
+start_par <-  data.frame(a = c(0.001), b=c(0.0001), c=c(0.1),A=c(0.1),B= c(0.1), freq=c(4.5), n=c(6), model_type=c("A") )  
+
+# execute the code
+temp <- run_analysis(x,y,start_par)
+
+# visualize the data and the model
+temp$plot
+
+
+
