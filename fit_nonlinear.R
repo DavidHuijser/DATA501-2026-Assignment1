51220e099f1700029ded07b08ed9eadc8ea47d49
DavidHuijser <davidhuijser@gmail.com>
2026-07-04 21:42:08 +1200

    simplified non-linear

--- fit_nonlinear.R
+++ fit_nonlinear.R
@@ -5,18 +5,20 @@ fit_nonlinear <- function(x, y, start_par) {
   print(paste("Model type", model_type))
   if (model_type == 'A')
   {
-      init_par <- c(start_par$a, start_par$b, start_par$c, start_par$A, start_par$B, start_par$freq)
-  }
-  
-  objective_function <- function(par) {
-    y_pred <- predict_model_A(x, par)
-    sum((y - y_pred)^2)
-  }
-  
-  result <- optim(init_par, objective_function)
-  if (model_type == 'A')
-  {
-    result$model <- predict_model_A(x, result$par)
+     # initialize
+     init_par <- c(start_par$a,start_par$b, start_par$c, start_par$A, start_par$B, start_par$freq)
+     
+     # objective function
+     objective_function <- function(par) {
+      y_pred <- predict_model_A(x, par)
+      sum((y - y_pred)^2)
+      }
+    
+     # optimize
+     result <- optim(init_par, objective_function)
+   
+     # get model
+     result$model <- predict_model_A(x, result$par)    
   }
   result
   
